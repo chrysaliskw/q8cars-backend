660522801ce89164105951ff7ab15f6ad8c47ac7
Fasna <fasna.pp@mindster.com>
2024-08-30 13:02:58 +0530

    car details api - fix

--- app/Http/Controllers/Api/User/CarController.php
+++ app/Http/Controllers/Api/User/CarController.php
@@ -485,10 +485,15 @@ class CarController extends ApiBaseController
 
     private function getMileageDetails(Car $car)
     {
-        $versions = CarVersion::select('*')->where('car_id', $car->id)
-                    ->groupBy('fuel_type', 'transmission_type')
-                    ->orderBy('id')
-                    ->get();
+        $subquery = CarVersion::select('fuel_type', 'transmission_type')
+            ->where('car_id', $car->id)
+            ->distinct()
+            ->toBase();
+
+        $versions = CarVersion::whereIn(DB::raw('(fuel_type, transmission_type)'), $subquery)
+            ->where('car_id', $car->id)
+            ->get();
+       
         return CarDetailResource::collection($versions);
 
     }
