f274eb0fe1d4852feeced8ab75560525c87d4685
ihsan haneef <ihsan.haneef@mindster.com>
2024-09-30 14:32:24 +0530

    banner completed

--- routes/admin.php
+++ routes/admin.php
@@ -83,6 +83,11 @@ Route::middleware('auth:admin')->group(function () {
     // Review
     Route::post('reviews/update', [ReviewController::class, 'update'])->name('reviews.update');
     Route::resource('reviews', ReviewController::class)->only(['index','show']);
+
+    //News
+    Route::post('news/banner', [NewsPostController::class, 'updatebanner'])->name('news.banner');
+
+    //Car Comparison
  
 
     //Trash 
