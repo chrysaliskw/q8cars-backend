6faad00799ff96f64f18b026176bf138197eec0b
Fasna P P- Mindster <fasna.pp@mindster.com>
2024-10-03 10:03:29 +0530

    compare cars from home

--- app/Models/CarComparisonList.php
+++ app/Models/CarComparisonList.php
@@ -11,5 +11,21 @@ class CarComparisonList extends Model
 
     const HOME_PAGE = 1;
     const CAR_DETAIL_PAGE = 2;
-    
+
+    public function car1()
+    {
+        return $this->belongsTo(Car::class,'car_1_id');
+    }
+    public function car2()
+    {
+        return $this->belongsTo(Car::class,'car_2_id');
+    }
+    public function carVersion1()
+    {
+        return $this->belongsTo(CarVersion::class,'car_version_1_id');
+    }
+    public function carVersion2()
+    {
+        return $this->belongsTo(CarVersion::class,'car_version_2_id');
+    }
 }
