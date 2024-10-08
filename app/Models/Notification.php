fc736476c289facb26685c18c075f2bd7ee00d3b
Fasna P P- Mindster <fasna.pp@mindster.com>
2024-10-08 19:00:31 +0530

    notification image file added

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -12,6 +12,8 @@ class Notification extends Model
     const STATUS_ACTIVE = 1;
     const STATUS_INACTIVE = 2;
     const STATUS_EXPIRED = 3;
+    
+    const FILE_DIR = 'notifications';
 
     protected $fillable = [
         'user_id',
