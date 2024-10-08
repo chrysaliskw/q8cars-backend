9c18d78d063534504082016597f6c9f1014375e3
Fasna P P- Mindster <fasna.pp@mindster.com>
2024-10-08 11:29:15 +0530

    compare cars list and compare car suggestion apis-migration change

--- database/migrations/2024_10_08_084826_add_column_body_type_id_to_car_comparison_lists.php
+++ database/migrations/2024_10_08_084826_add_column_body_type_id_to_car_comparison_lists.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::table('car_comparison_lists', function (Blueprint $table) {
            $table->bigInteger('body_type_id')->after('brand_id')->nullable();
+           $table->bigInteger('view_count')->after('car_version_2_id')->default(0);
         });
     }
 
