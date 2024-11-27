095db8ea2fd49cfab1c5f239aa021ea1377264b8
Nabeel Cryf T <nabeel.cryf@mindster.com>
2024-11-27 15:55:14 +0530

    Modified car comparison details API

--- app/Http/Controllers/Api/User/CompareCarsDetailsController.php
+++ app/Http/Controllers/Api/User/CompareCarsDetailsController.php
@@ -5,9 +5,10 @@ namespace App\Http\Controllers\Api\User;
 use App\Models\Car;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use App\Http\Controllers\Api\ApiBaseController;
-use Illuminate\Support\Facades\Validator;
 use App\Models\BrandColorMapping;
+use Illuminate\Support\Facades\Validator;
+use App\Models\CarAdditonalSpecifications;
+use App\Http\Controllers\Api\ApiBaseController;
 
 class CompareCarsDetailsController extends ApiBaseController
 {
@@ -16,20 +17,198 @@ class CompareCarsDetailsController extends ApiBaseController
      */
     public function __invoke(Request $request)
     {
-       
-
        $validator =   Validator::make($request->all(), [
             'carIds' => 'required|array',
             'carIds.*' => 'integer|exists:cars,id',
+            'is_common' => 'nullable|in:1',
+            'is_different' => 'nullable|in:1'
         ]);
+        // dd($request->all());
         if ($validator->fails()) {
             return $this->error($validator->errors()->first(), Response::HTTP_UNPROCESSABLE_ENTITY);
         }
-        
+
         $carIds = $request->carIds;
-        $cars = Car::whereIn('id',$carIds)->get();
+        $isCommon = $request->is_common;
+        $isDifferent = $request->is_different;
+        $cars = Car::whereIn('id',$carIds)->orderByRaw('FIELD(id, ' . implode(',', $carIds) . ')')
+        ->get();
+
+        if($isCommon === '1'){
+            $data = $this->getCommonCarComparison($cars);
+            return $this->success(['data' => $data], 'Common Comparison Details!', Response::HTTP_OK);
+        }
+
+        if($isDifferent === '1'){
+            $data = $this->getDifferentCarComparison($cars, $carIds);
+            return $this->success(['data' => $data], 'Different Comparison Details!', Response::HTTP_OK);
+        }
+
+        $specifications = [];
+
+        $basicInfo = $this->basicInfo($cars);
+        $colorInfo = $this->colorInfo($cars);
+        $engineInfo = $this->engineInfo($cars);
+        $fuelInfo = $this->fuelInfo($cars);
+        $suspensionInfo = $this->suspensionInfo($cars);
+        $dimensionInfo = $this->dimensionInfo($cars);
+        $comfortInfo = $this->comfortInfo($cars);
+        $interiorInfo = $this->interiorInfo($cars);
+        $exteriorInfo = $this->exteriorInfo($cars);
+        $safetyInfo = $this->safetyInfo($cars);
+        $entertainmentInfo = $this->entertainmentInfo($cars);
+
+        if (!is_array($basicInfo)) {
+            $basicInfo = [];
+        }
+        $specifications['basic_information'] = $basicInfo;
+
+        if (!is_array($colorInfo)) {
+            $colorInfo = [];
+        }
+        $specifications['colors'] = $colorInfo;
+
+        if (!is_array($engineInfo)) {
+            $engineInfo = [];
+        }
+        $specifications['engine_tranmission'] = $engineInfo;
+
+        if (!is_array($fuelInfo)) {
+            $fuelInfo = [];
+        }
+        $specifications['fuel_performance'] = $fuelInfo;
+
+        if (!is_array($suspensionInfo)) {
+            $suspensionInfo = [];
+        }
+        $specifications['suspension_steering'] = $suspensionInfo;
+
+        if (!is_array($dimensionInfo)) {
+            $dimensionInfo = [];
+        }
+        $specifications['dimension_capacity'] = $dimensionInfo;
+
+        if (!is_array($comfortInfo)) {
+            $comfortInfo = [];
+        }
+        $specifications['comfort_convenience'] = $comfortInfo;
+
+        if (!is_array($interiorInfo)) {
+            $interiorInfo = [];
+        }
+        $specifications['interior'] = $interiorInfo;
+
+        if (!is_array($exteriorInfo)) {
+            $exteriorInfo = [];
+        }
+        $specifications['exterior'] = $exteriorInfo;
+
+        if (!is_array($safetyInfo)) {
+            $safetyInfo = [];
+        }
+        $specifications['safety'] = $safetyInfo;
+
+        if (!is_array($entertainmentInfo)) {
+            $entertainmentInfo = [];
+        }
+        $specifications['entertainment'] = $entertainmentInfo;
+
+
+        foreach ($cars as $key => $car) {
+            $carSpecifications = CarAdditonalSpecifications::where('car_id', $car->id)
+                ->select('specification', 'unit', 'value', 'category_id')
+                ->get();
+
+            if ($carSpecifications->isEmpty()) {
+                continue;
+            }
+
+            $carSpecifications->map(function($spec) use (&$specifications, $key) {
+                $category = $this->getCategoryName($spec->category_id);
+
+                if (!isset($specifications[$category])) {
+                    $specifications[$category] = [];
+                }
+
+                if (!isset($specifications[$category][$spec->specification])) {
+                    $specifications[$category][$spec->specification] = [];
+                }
+
+                $value = trim($spec->value . ' ' . $spec->unit);
+                if (empty($spec->unit)) {
+                    $value = $spec->value;
+                }
+
+                $carKey = 'car_' . $key;
+                $specifications[$category][$spec->specification][$carKey] = $value;
+            });
+        }
+
+        $carCount = count($cars);
+
+
+        foreach ($specifications as $category => &$specCategory) {
+            foreach ($specCategory as $specification => &$specValue) {
+                for ($i = 0; $i < $carCount; $i++) {
+                    $carKey = 'car_' . $i;
+                    if (!isset($specValue[$carKey])) {
+                        $specValue[$carKey] = "";
+                    }
+                }
+            }
+        }
+
+        $colorInfoByCar = [];
+
+        foreach ($cars as $key => $car) {
+            $carKey = 'car_' . $key;
+
+            if (!isset($colorInfoByCar[$carKey])) {
+                $colorInfoByCar[$carKey] = [];
+            }
+
+            foreach (json_decode($car->colours) as $type) {
+                $map = BrandColorMapping::find($type);
+                if (isset($map)) {
+                    $colorInfoByCar[$carKey][$type] = $map->code;
+                }
+            }
+        }
+
+        $specifications['colors'] = $colorInfoByCar;
+
         // $data =   $this->getCarComparison($cars);
-        $data = [
+        $data = $specifications;
+        return $this->success(['data' =>  $data], 'comparison Details!', Response::HTTP_OK);
+    }
+
+    private function getCategoryName($categoryId) {
+        switch ($categoryId) {
+            case CarAdditonalSpecifications::CATEGORY_ENGINE:
+                return 'engine_tranmission';
+            case CarAdditonalSpecifications::CATEGORY_FUEL:
+                return 'fuel_performance';
+            case CarAdditonalSpecifications::CATEGORY_SUSPENSION:
+                return 'suspension_steering';
+            case CarAdditonalSpecifications::CATEGORY_DIMENSION:
+                return 'dimension_capacity';
+            case CarAdditonalSpecifications::CATEGORY_EXTERIOR:
+                return 'exterior';
+            case CarAdditonalSpecifications::CATEGORY_INTERIOR:
+                return 'interior';
+            case CarAdditonalSpecifications::CATEGORY_SAFETY:
+                return 'safety';
+            case CarAdditonalSpecifications::CATEGORY_ENTERTAINMENT:
+                return 'entertainment';
+            case CarAdditonalSpecifications::CATEGORY_COMFORT:
+                return 'comfort_convenience';
+            default:
+                return 'Unknown Category';
+        }
+    }
+
+    private function getCommonCarComparison($cars)    {
+        $specifications =  [
             'basic_information'     => $this->basicInfo($cars),
             'colors'                => $this->colorInfo($cars),
             'engine_tranmission'    => $this->engineInfo($cars),
@@ -40,25 +219,105 @@ class CompareCarsDetailsController extends ApiBaseController
             'interior'              => $this->interiorInfo($cars),
             'exterior'              => $this->exteriorInfo($cars),
             'safety'                => $this->safetyInfo($cars),
-            'entertainment'        => $this->entertainmentInfo($cars),    
+            'entertainment'        => $this->entertainmentInfo($cars),
         ];
-        return $this->success(['data' =>  $data], 'comparison Details!', Response::HTTP_OK);
+
+        $colorInfoByCar = [];
+
+        foreach ($cars as $key => $car) {
+            $carKey = 'car_' . $key;
+
+            if (!isset($colorInfoByCar[$carKey])) {
+                $colorInfoByCar[$carKey] = [];
+            }
+
+            foreach (json_decode($car->colours) as $type) {
+                $map = BrandColorMapping::find($type);
+                if (isset($map)) {
+                    $colorInfoByCar[$carKey][$type] = $map->code;
+                }
+            }
+        }
+
+        $specifications['colors'] = $colorInfoByCar;
+        return $specifications;
     }
 
-    private function getCarComparison($cars)    {
-      return [
-        'basic information'     => $this->basicInfo($cars),
-        'colors'                => $this->colorInfo($cars),
-        'engine and tranmission'    => $this->engineInfo($cars),
-        'fuel and performance'      => $this->fuelInfo($cars),
-        'suspension and steering'   => $this->suspensionInfo($cars),
-        'dimension and capacity'    => $this->dimensionInfo($cars),
-        'comfort and convenience'   => $this->comfortInfo($cars),
-        'interior'              => $this->interiorInfo($cars),
-        'exterior'              => $this->exteriorInfo($cars),
-        'safety'                => $this->safetyInfo($cars),
-        'entertainment'        => $this->entertainmentInfo($cars),    
-      ];
+    private function getDifferentCarComparison($cars, $carIds) {
+        $specifications = [];
+        $carKeyMap = [];
+
+        foreach ($carIds as $index => $carId) {
+            $carKeyMap[$carId] = 'car_' . $index;
+        }
+
+        $categories = [
+            'basic_information'     => [],
+            'colors'                => [],
+            'engine_tranmission' => $this->engineInfo($cars),
+            'fuel_performance' => $this->fuelInfo($cars),
+            'suspension_steering' => $this->suspensionInfo($cars),
+            'dimension_capacity' => $this->dimensionInfo($cars),
+            'comfort_convenience' => $this->comfortInfo($cars),
+            'interior' => $this->interiorInfo($cars),
+            'exterior' => $this->exteriorInfo($cars),
+            'safety' => $this->safetyInfo($cars),
+            'entertainment' => $this->entertainmentInfo($cars),
+        ];
+
+        foreach ($categories as $category => $data) {
+            $specifications[$category] = is_array($data) ? $data : [];
+        }
+
+        foreach ($cars as $car) {
+            $carSpecifications = CarAdditonalSpecifications::where('car_id', $car->id)
+                ->select('specification', 'unit', 'value', 'category_id')
+                ->get();
+
+            foreach ($carSpecifications as $spec) {
+                $category = $this->getCategoryName($spec->category_id);
+
+                if (!isset($specifications[$category])) {
+                    $specifications[$category] = [];
+                }
+
+                if (!isset($specifications[$category][$spec->specification])) {
+                    $specifications[$category][$spec->specification] = [];
+                }
+
+                $value = trim($spec->value . ' ' . $spec->unit);
+                if (empty($spec->unit)) {
+                    $value = $spec->value;
+                }
+
+                $carKey = $carKeyMap[$car->id];
+                $specifications[$category][$spec->specification][$carKey] = $value;
+            }
+        }
+
+        foreach ($specifications as $category => &$specCategory) {
+            foreach ($specCategory as $specification => &$specValues) {
+                foreach ($carKeyMap as $carKey) {
+                    if (!isset($specValues[$carKey])) {
+                        $specValues[$carKey] = "";
+                    }
+                }
+            }
+        }
+
+        foreach ($specifications as $category => &$specCategory) {
+            foreach ($specCategory as $specification => &$specValues) {
+                $values = array_values($specValues);
+
+                $nonEmptyValues = array_filter($values, fn($val) => !empty($val));
+
+                if (count($nonEmptyValues) > 1) {
+                    unset($specCategory[$specification]);
+                }
+            }
+        }
+
+        return $specifications;
     }
 
     private function basicInfo($cars)
@@ -75,7 +334,7 @@ class CompareCarsDetailsController extends ApiBaseController
         return $comparisonData;
     }
     private function colorInfo($cars)
-    { 
+    {
         $colours = [];
         foreach($cars as $car){
             foreach (json_decode($car->colours) as $type) {
@@ -105,7 +364,7 @@ class CompareCarsDetailsController extends ApiBaseController
                 }
             }
         }
-        ksort($engineData);      
+        ksort($engineData);
         return $this->mergeSimilarLabels($engineData, $carCount);
     }
 
@@ -132,7 +391,7 @@ class CompareCarsDetailsController extends ApiBaseController
     {
         $suspensionData = [];
         $carCount = $cars->count();
-        foreach ($cars as $key => $car) {          
+        foreach ($cars as $key => $car) {
              if (isset($car->carSpec->suspensionData)) {
                 foreach ($car->carSpec->fuel as $spec) {
                     $specLabelLower = strtolower($spec->specification);
@@ -149,7 +408,7 @@ class CompareCarsDetailsController extends ApiBaseController
     {
         $dimensionData = [];
         $carCount = $cars->count();
-        foreach ($cars as $key => $car) {  
+        foreach ($cars as $key => $car) {
              $dimensionData['body type']["car_$key"] =  $car->carSpec->bodyType->name;
              if (isset($car->carSpec->dimension)) {
                 foreach ($car->carSpec->dimension as $spec) {
@@ -187,7 +446,7 @@ class CompareCarsDetailsController extends ApiBaseController
         $interiorData = [];
         $carCount = $cars->count();
         foreach ($cars as $key => $car) {
-           
+
              if (isset($car->carSpec->interior)) {
                 foreach ($car->carSpec->interior as $spec) {
                     $specLabelLower = strtolower($spec->specification);
@@ -206,7 +465,7 @@ class CompareCarsDetailsController extends ApiBaseController
         $exteriorData = [];
         $carCount = $cars->count();
         foreach ($cars as $key => $car) {
-           
+
              if (isset($car->carSpec->exterior)) {
                 foreach ($car->carSpec->exterior as $spec) {
                     $specLabelLower = strtolower($spec->specification);
@@ -227,7 +486,7 @@ class CompareCarsDetailsController extends ApiBaseController
         foreach ($cars as $key => $car) {
             $safetyData['no of airbags']["car_$key"] =  $car->carSpec->no_of_airbags;
             $safetyData['safety ratings']["car_$key"] =  $car->carSpec->safety_ratings;
-            
+
              if (isset($car->carSpec->safety)) {
                 foreach ($car->carSpec->safety as $spec) {
                     $specLabelLower = strtolower($spec->specification);
@@ -254,11 +513,11 @@ class CompareCarsDetailsController extends ApiBaseController
                 }
             }
         }
-    
-        ksort($entertainmentData); 
+
+        ksort($entertainmentData);
         return $this->mergeSimilarLabels($entertainmentData, $carCount);
     }
-    
+
     private function appendValue(&$data, $formattedLabel, $car, $spec, $key)
     {
         if (isset($data[$formattedLabel]["car_$key"])) {
@@ -266,7 +525,7 @@ class CompareCarsDetailsController extends ApiBaseController
         }
         return $spec->value . $spec->unit;
     }
-    
+
     private function mergeSimilarLabels(&$data, $carCount)
     {
         $keys = array_keys($data);
@@ -302,7 +561,7 @@ class CompareCarsDetailsController extends ApiBaseController
 
         return $data;
     }
- 
-   
-   
+
+
+
 }
