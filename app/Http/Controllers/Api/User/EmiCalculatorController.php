3603855e71bfe11cf118668d282c9dba3e92b6a5
Nabeel Cryf T <nabeel.cryf@mindster.com>
2024-11-28 15:51:23 +0530

    emi calc API on road price validation

--- app/Http/Controllers/Api/User/EmiCalculatorController.php
+++ app/Http/Controllers/Api/User/EmiCalculatorController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers\Api\User;
 
+use App\Models\Car;
+use App\Models\EmiInfo;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use App\Services\Common\EmiCalculatorService;
 use App\Http\Controllers\Api\ApiBaseController;
-use App\Models\EmiInfo;
 
 class EmiCalculatorController extends ApiBaseController
 {
@@ -29,13 +30,21 @@ class EmiCalculatorController extends ApiBaseController
             return $this->error($validator->errors()->first(), Response::HTTP_UNPROCESSABLE_ENTITY);
         }
 
+        $car = Car::find($request->car_id);
+        $on_road_price = $car->on_road_price;
+
+        if ($request->principal > $on_road_price) {
+            return $this->error(
+                "The principal amount cannot be greater than the on-road price of the car. On-road price: KWD $on_road_price", Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
         if(!isset($request->principal) || !isset($request->annualInterestRate) || !isset($request->loanTenureYears)) {
             $request->merge($this->getCarBaseEmiCalcualtions($request->car_id, $request->car_version_id));
         }
-        
+
         $service = new EmiCalculatorService($request);
         $result = $service->handle();
-       
+
         return $this->success(['data' => $result], 'EMI Calculator!', Response::HTTP_OK);
     }
 
