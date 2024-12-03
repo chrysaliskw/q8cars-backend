5359769eb0d7678bd8c9e3098da7b24c1df573db
Fasna P P- Mindster <fasna.pp@mindster.com>
2024-12-03 13:13:52 +0530

    fix

--- routes/admin.php
+++ routes/admin.php
@@ -134,6 +134,11 @@ Route::middleware(['auth:admin', 'role_or_permission:Super Admin|Loan Eligibilit
 });
 //Car Management
 Route::middleware(['auth:admin', 'role_or_permission:Super Admin|Car Management'])->group(function () {
+    Route::get('car/add-360-view', [CarController::class, 'add360ViewImages'])->name('car.360-view.add');
+    Route::post('car/store-360-view', [CarController::class, 'store360ViewImages'])->name('car.360-view.store');
+    Route::post('car/delete-360-view', [CarController::class, 'delete360ViewImage'])->name('car.360-view.delete');
+    Route::post('car/update-360-view', [CarController::class, 'update360ViewImage'])->name('car.360-view.update');
+   
     Route::resources([
         'car' => CarController::class,                  // Car
         'car-version' => CarVersionController::class,   // Car Version
