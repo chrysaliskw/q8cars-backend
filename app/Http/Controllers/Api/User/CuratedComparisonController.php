f302db644e810cf8d68283f5d552ae1500d6d9fb
ihsan haneef <ihsan.haneef@mindster.com>
2024-12-09 10:14:17 +0530

    published date in curated

--- app/Http/Controllers/Api/User/CuratedComparisonController.php
+++ app/Http/Controllers/Api/User/CuratedComparisonController.php
@@ -10,19 +10,21 @@ use App\Models\CuratedComparison;
 
 class CuratedComparisonController extends ApiBaseController
 {
-    
-public function __invoke(Request $request)
-    {
-       $query = CuratedComparison::active()->orderBy('id','Desc');
-       $comarisons = $query->limit(3)->get();
 
-       if($request->is_paginate == 1)
-       {
-        $comarisons = $query->paginate(10);
-      
-       }
+    public function __invoke(Request $request)
+    {
+        // Get all curated comparisons that are active and have a published date less than or equal to today
+        // Order them by id in descending order (newest first)
+        $query = CuratedComparison::active()
+            ->orderBy('id', 'Desc')
+            ->where('published_date', '<=', date('Y-m-d'));
+        $comarisons = $query->limit(3)->get();
+
+        if ($request->is_paginate == 1) {
+            $comarisons = $query->paginate(10);
+        }
         return $this->success([
-           'data' =>  CuratedComparisonResource::collection($comarisons)
+            'data' =>  CuratedComparisonResource::collection($comarisons)
         ], 'Curated Comparisons listing', Response::HTTP_OK);
     }
 }
