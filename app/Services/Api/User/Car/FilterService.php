6ace11d04c914cfef00fd2b00afd422534354c74
Fasna P P- Mindster <fasna.pp@mindster.com>
2024-12-13 16:00:34 +0530

    fix

--- app/Services/Api/User/Car/FilterService.php
+++ app/Services/Api/User/Car/FilterService.php
@@ -205,7 +205,7 @@ final class FilterService
         //         ->pluck('car_id')
         //         ->toArray();
         // $this->query = $this->query->whereIn('cars.id', $ids);
-        $this->query = $this->query->whereBetween('cars.on_road_price', [
+        $this->query = $this->query->whereBetween('cars.ex_showroom_price', [
                 $this->request->min_price, $this->request->max_price
         ]);
     }
