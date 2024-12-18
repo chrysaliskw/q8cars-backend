9ffa29148cc1c2ede5abb5e24f2a552385edede1
ihsan haneef <ihsan.haneef@mindster.com>
2024-12-18 12:23:04 +0530

    completed fixx

--- resources/views/admin/car/edit-section/edit_videos.blade.php
+++ resources/views/admin/car/edit-section/edit_videos.blade.php
@@ -1,54 +1,82 @@
 <!-- Container for all sections -->
- <!-- Add Button -->
+<!-- Add Button -->
 <div class="text-center mt-3">
-    <button type="button" class="btn btn-primary" onclick="addFormSection()" style="margin-left: 800px;">+ Add Video</button>
+    <button type="button" class="btn btn-primary" onclick="addFormSection()" style="margin-left: 800px;">+ Add
+        Video</button>
 </div>
 <div id="form-sections-container">
     @php
         $carVideoCount = $carVideos->count();
     @endphp
     @foreach ($carVideos as $key => $carVideo)
-    <div class="row form-section" id="form-section-{{ $key + 1 }}">
-        <div class="col-md-4">
-            <x-form-input type="text" field="title_{{ $key + 1 }}" field-name="Title" value="{{ $carVideo->video_title }}"></x-form-input>
-        </div>
-        <div class="col-md-4">
-            <x-form-textarea type="text" field="description_{{ $key + 1 }}" field-name="Description" field-value="{{ $carVideo->video_description }}"></x-form-textarea>
-        </div>
-        <div class="col-md-4">
-            <x-form-input type="text" field="posted_media_{{ $key + 1 }}" field-name="Posted Media" value="{{ $carVideo->video_posted_media }}"></x-form-input>
-        </div>
-        <div class="col-md-4">
-            <div class="form-group">
-                <label for="date_{{ $key + 1 }}" class="control-label">Posted Date</label>
-                <div class="input-group">
-                    <input type="text" name="date_{{ $key + 1 }}" id="date_{{ $key + 1 }}" class="form-control"
-                        value="{{ $carVideo->video_posted_date }}">
-                    <div class="input-group-append">
-                        <span class="input-group-text"><i class="md md-event"></i></span>
+        <div class="row form-section" id="form-section-{{ $key + 1 }}">
+            <div class="col-md-4">
+                <x-form-input type="text" field="title_{{ $key + 1 }}" field-name="Title"
+                    value="{{ $carVideo->video_title }}"></x-form-input>
+            </div>
+            <div class="col-md-4">
+                <x-form-textarea type="text" field="description_{{ $key + 1 }}" field-name="Description"
+                    field-value="{{ $carVideo->video_description }}"></x-form-textarea>
+            </div>
+            <div class="col-md-4">
+                <x-form-input type="text" field="posted_media_{{ $key + 1 }}" field-name="Posted Media"
+                    value="{{ $carVideo->video_posted_media }}"></x-form-input>
+            </div>
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label for="date_{{ $key + 1 }}" class="control-label">Posted Date</label>
+                    <div class="input-group">
+                        <input type="text" name="date_{{ $key + 1 }}" id="date_{{ $key + 1 }}"
+                            class="form-control" value="{{ $carVideo->video_posted_date }}">
+                        <div class="input-group-append">
+                            <span class="input-group-text"><i class="md md-event"></i></span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-4">
-            <div class="form-group">
-                <label for="thumbnail_{{ $key + 1 }}" class="control-label">Thumbnail</label><br>
-                @if($carVideo->thumbnail)
-                <img src="{{ file_asset('files-car', $carVideo->thumbnail) }}" alt="brand-img" class="img-thumbnail" width="100" height="150">
-                @endif
-                <input id="thumbnail_{{ $key + 1 }}" type="file" name="thumbnail_{{ $key + 1 }}" class="form-control">
-                @error('thumbnail_'.($key+1))
-                    <span class="error" role="alert">{{ $message }}</span>
-                @enderror
-                <span class="text-muted">Max size: 2MB</span>
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label for="thumbnail_{{ $key + 1 }}" class="control-label">Thumbnail</label><br>
+                    @if ($carVideo->thumbnail)
+                        <img src="{{ file_asset('files-car', $carVideo->thumbnail) }}" alt="brand-img"
+                            class="img-thumbnail" width="100" height="150">
+                    @endif
+                    <input id="thumbnail_{{ $key + 1 }}" type="file" name="thumbnail_{{ $key + 1 }}"
+                        class="form-control">
+                    @error('thumbnail_' . ($key + 1))
+                        <span class="error" role="alert">{{ $message }}</span>
+                    @enderror
+                    <span class="text-muted">Max size: 2MB</span>
+                </div>
+            </div>
+            {{-- <div class="col-md-4">
+                <x-form-input type="file" field="video_{{ $key + 1 }}" field-name="Video File"
+                    value=""></x-form-input>
+                <span class="text-muted">MP4 only, Max size: 2MB</span>
+            </div> --}}
+            <div class="col-md-4">
+                <div class="form-group">
+
+
+
+                    <label for="video_${sectionCount}" class="control-label">Video File</label><br>
+                    @if($carVideo->file_name)
+                    <video  width="100" height="150" controls class="img-thumbnail">
+                        <source src="{{ file_asset('files-car', $carVideo->file_name) }}" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                    @endif
+                    <input id="video_${sectionCount}" type="file" name="video_${sectionCount}" class="form-control"
+                        onchange="previewVideo(event, ${sectionCount})">
+                    <span class="error" role="alert">
+                        @error('video_' . ($key + 1))
+                            {{ $message }}</br>
+                        @enderror
+                    </span>
+                </div>
             </div>
         </div>
-        <div class="col-md-4">
-            <x-form-input type="file" field="video_{{ $key + 1 }}" field-name="Video File" value=""></x-form-input>
-            <span class="text-muted">MP4 only, Max size: 2MB</span>
-        </div>
-    </div>
-    <hr><br>
+        <hr><br>
     @endforeach
 </div>
 
@@ -97,33 +125,16 @@
                     <x-form-input type="file" field="thumbnail_${sectionCount}" field-name="Thumbnail" value=""></x-form-input>
                     <span class="text-muted">Max size: 2MB</span>
                 </div>
-<div class="col-md-4">
-        <div class="form-group">
-        <label for="video_${sectionCount}" class="control-label">Video File</label><br>
-        @if($carVideo->file_name)
-
-            <video  width="100" height="150" controls autoplay class="img-thumbnail">
-                <source src="{{ file_asset('files-car', $carVideo->file_name) }}" type="video/mp4">
-                Your browser does not support the video tag.
-            </video>
-        @endif
-        {{-- <x-form-input
-            type="file"
-            field="video_${sectionCount}"
-            field-name=""
-            >
-
-        </x-form-input> --}}
-        <input id="video_${sectionCount}" type="file" name="video_${sectionCount}" class="form-control ">
-        <span class="error" role="alert">
-            @error('video_'.($key+1))
-                {{ $message }}</br>
-            @enderror
-        </span>
-        </div>
-
-    </div>
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label for="video_${sectionCount}" class="control-label">Video File</label><br>
+                        <input id="video_${sectionCount}" type="file" name="video_${sectionCount}" class="form-control"
+                            onchange="previewVideo(event, ${sectionCount})">
+                       
+                    </div>
+                </div>
             </div>
+        </div>
              <hr><br>
         `;
 
