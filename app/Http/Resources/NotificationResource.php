ee65782a183642277d546b17afb1a98045bd3368
Fasna P P- Mindster <fasna.pp@mindster.com>
2024-12-19 11:00:17 +0530

    fix

--- app/Http/Resources/NotificationResource.php
+++ app/Http/Resources/NotificationResource.php
@@ -12,19 +12,20 @@ class NotificationResource extends JsonResource
 {
     public function toArray($request)
     {
-      
-        $readStatus = UserNotificationMapping::where('user_id',Auth::id())->where('notification_id',$this->id)->read_status;
-
+        $userNotificationMapping = UserNotificationMapping::where('user_id', Auth::id())
+        ->where('notification_id', $this->id)
+        ->first();
+        $readStatus = $userNotificationMapping ? $userNotificationMapping->read_status : 1;
         return [
-            'id' => $this->id,
-            'title' => $this->title,
-            'description' => $this->description,
-            'image' => file_asset('files-notifications', $this->image),
-            'logo' => file_asset('files-notifications', $this->logo),
-            'business_name' => $this->business_name,
-            'status' => $this->status,
-            'read_status' => $readStatus,
-            'created_at' => $this->created_at->diffForHumans(),
-        ];
-    }
+                    'id' => $this->id,
+                    'title' => $this->title,
+                    'description' => $this->description,
+                    'image' => file_asset('files-notifications', $this->image),
+                    'logo' => file_asset('files-notifications', $this->logo),
+                    'business_name' => $this->business_name,
+                    'status' => $this->status,
+                    'read_status' => $readStatus,
+                    'created_at' => $this->created_at->diffForHumans(),
+                ];
+            }
 }
