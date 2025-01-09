cce0438815c4f048281572efc181ea61e68e7e71
ihsan haneef <ihsan.haneef@mindster.com>
2025-01-09 17:17:15 +0530

    key added

--- app/Http/Resources/CarResource.php
+++ app/Http/Resources/CarResource.php
@@ -19,6 +19,7 @@ class CarResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'key_id' => $this->id,
             'brand_id' => $this->brand_id,
             'brand_name' => $this->brand->name,
             'name' => $this->model_name,
