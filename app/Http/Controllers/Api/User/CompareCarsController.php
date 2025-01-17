604bfdf6f770531b0706695fc8e80b3d8aa62f89
Fasna P P- Mindster <fasna.pp@mindster.com>
2025-01-17 16:21:08 +0530

    fix

--- app/Http/Controllers/Api/User/CompareCarsController.php
+++ app/Http/Controllers/Api/User/CompareCarsController.php
@@ -127,7 +127,7 @@ class CompareCarsController extends ApiBaseController
         $car2Ids = RecentComparison::where('user_id', Auth::id())->pluck('car_2_id')->toArray();
         $uniqueCarIds = array_unique(array_merge($car1Ids, $car2Ids));
         // $suggestions = Car::whereNotIn('id', $uniqueCarIds)->active()->orderBy('view_count')->limit(3)->get();
-        $suggestions = Car::whereNotIn('id', $uniqueCarIds)->active()->orderByDesc('view_count')->limit(3)->get();
+        $suggestions = Car::whereNotIn('id', $uniqueCarIds)->active()->orderByDesc('view_count')->launched()->limit(3)->get();
         return $this->success(['data' => CarSuggestionResource::collection($suggestions)], 'Compare Cars suggestions!', Response::HTTP_OK);
     }
 }
