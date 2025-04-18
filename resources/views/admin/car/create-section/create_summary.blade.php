e96738d3d46d982adb0c85f14d839d30c809a54d
Akkuekku@2992 <jasmin.n@mindster.com>
2025-04-18 21:51:52 +0530

    remobve star symbol from blade pages

--- resources/views/admin/car/create-section/create_summary.blade.php
+++ resources/views/admin/car/create-section/create_summary.blade.php
@@ -1,29 +1,29 @@
 <div class="row">
     <div class="col-md-6">
-        <x-form-textarea field="why_choose" field-name="Why choose this car?*" rows="6" field-value="{{ old('why_choose') }}"></x-form-textarea>
+        <x-form-textarea field="why_choose" field-name="Why choose this car?" rows="6" field-value="{{ old('why_choose') }}"></x-form-textarea>
     </div>
     <div class="col-md-6">
-        <x-form-textarea type="text" field="market_introduction" rows="6" field-name="Market Introduction*" field-value="{{ old('market_introduction') }}">
+        <x-form-textarea type="text" field="market_introduction" rows="6" field-name="Market Introduction" field-value="{{ old('market_introduction') }}">
         </x-form-textarea>
     </div>
     <div class="col-md-6">
-        <x-form-textarea type="text" field="engine_transmission" rows="6" field-name="Engine and Transmission*" field-value="{{ old('engine_transmission') }}">
+        <x-form-textarea type="text" field="engine_transmission" rows="6" field-name="Engine and Transmission" field-value="{{ old('engine_transmission') }}">
         </x-form-textarea>
     </div>
     <div class="col-md-6">
-        <x-form-textarea type="text" field="exterior" rows="6" field-name="Exterior*" field-value="{{ old('exterior') }}">
+        <x-form-textarea type="text" field="exterior" rows="6" field-name="Exterior" field-value="{{ old('exterior') }}">
         </x-form-textarea>
     </div>
     <div class="col-md-6">
-        <x-form-textarea type="text" field="interior" rows="6" field-name="Interior*" field-value="{{ old('interior') }}">
+        <x-form-textarea type="text" field="interior" rows="6" field-name="Interior" field-value="{{ old('interior') }}">
         </x-form-textarea>
     </div>
     <div class="col-md-6">
-        <x-form-textarea type="text" field="safety_features" rows="6" field-name="Safety Features*" field-value="{{ old('safety_features') }}">
+        <x-form-textarea type="text" field="safety_features" rows="6" field-name="Safety Features" field-value="{{ old('safety_features') }}">
         </x-form-textarea>
     </div>
     <div class="col-md-6">
-        <x-form-textarea type="text" field="mileage_summary" rows="6" field-name="Mileage Summary*" field-value="{{ old('mileage_summary') }}">
+        <x-form-textarea type="text" field="mileage_summary" rows="6" field-name="Mileage Summary" field-value="{{ old('mileage_summary') }}">
         </x-form-textarea>
     </div>
     <div class="col-md-6">
--- resources/views/admin/car/edit-section/edit_summary.blade.php
+++ resources/views/admin/car/edit-section/edit_summary.blade.php
@@ -1,29 +1,29 @@
 <div class="row">
     <div class="col-md-6">
-        <x-form-textarea field="why_choose" field-name="Why choose this car?*" rows="6" field-value="{{ $car->why_choose }}"></x-form-textarea>
+        <x-form-textarea field="why_choose" field-name="Why choose this car?" rows="6" field-value="{{ $car->why_choose }}"></x-form-textarea>
     </div>
     <div class="col-md-6">
-        <x-form-textarea type="text" field="market_introduction" rows="6" field-name="Market Introduction*" field-value="{{  $car->market_introduction }}">
+        <x-form-textarea type="text" field="market_introduction" rows="6" field-name="Market Introduction" field-value="{{  $car->market_introduction }}">
         </x-form-textarea>
     </div>
     <div class="col-md-6">
-        <x-form-textarea type="text" field="engine_transmission" rows="6" field-name="Engine and Transmission*" field-value="{{  $car->engine_transmission }}">
+        <x-form-textarea type="text" field="engine_transmission" rows="6" field-name="Engine and Transmission" field-value="{{  $car->engine_transmission }}">
         </x-form-textarea>
     </div>
     <div class="col-md-6">
-        <x-form-textarea type="text" field="exterior" rows="6" field-name="Exterior*" field-value="{{  $car->exterior }}">
+        <x-form-textarea type="text" field="exterior" rows="6" field-name="Exterior" field-value="{{  $car->exterior }}">
         </x-form-textarea>
     </div>
     <div class="col-md-6">
-        <x-form-textarea type="text" field="interior" rows="6" field-name="Interior*" field-value="{{  $car->interior }}">
+        <x-form-textarea type="text" field="interior" rows="6" field-name="Interior" field-value="{{  $car->interior }}">
         </x-form-textarea>
     </div>
     <div class="col-md-6">
-        <x-form-textarea type="text" field="safety_features" rows="6" field-name="Safety Features*" field-value="{{  $car->safety_features }}">
+        <x-form-textarea type="text" field="safety_features" rows="6" field-name="Safety Features" field-value="{{  $car->safety_features }}">
         </x-form-textarea>
     </div>
     <div class="col-md-6">
-        <x-form-textarea type="text" field="mileage_summary" rows="6" field-name="Mileage Summary*" field-value="{{  $car->mileage_summary }}">
+        <x-form-textarea type="text" field="mileage_summary" rows="6" field-name="Mileage Summary" field-value="{{  $car->mileage_summary }}">
         </x-form-textarea>
     </div>
     <div class="col-md-6">
