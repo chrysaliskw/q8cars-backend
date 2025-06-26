1e3f375d54e5344bf7a6d26629745375152317e6
Nabeel Cryf T <nabeel.cryf@mindster.com>
2025-06-26 17:20:34 +0530

    unit change based on fuel type

--- app/Http/Resources/CarDetailResource.php
+++ app/Http/Resources/CarDetailResource.php
@@ -15,13 +15,23 @@ class CarDetailResource extends JsonResource
    */
   public function toArray($request)
   {
+    $fuelType = $this->fuel_type;
+    $engineUnit = in_array($fuelType, [1, 2, 3, 5]) ? 'L' : ($fuelType == 4 ? 'kW' : 'cc');
+    $mileageUnit = ($fuelType == 4) ? 'kWh' : 'kmpl';
+
     return [
         'id' => $this->id,
         'version_name' => $this->varient_name,
-        'transmission_type' => config('params.car.transmission_type')[$this->transmission_type],  
+        'transmission_type' => config('params.car.transmission_type')[$this->transmission_type],
         'fuel_type' => config('params.car.fuel_type')[$this->fuel_type],
-        'engine_capacity' => $this->engine_capacity. ' cc',
-        'mileage' => $this->mileage ? ($this->fuel_type == 4 ? $this->mileage. ' kwh':$this->mileage. ' kmpl') :'',
+        // 'engine_capacity' => $this->engine_capacity. ' cc',
+        // 'mileage' => $this->mileage ? ($this->fuel_type == 4 ? $this->mileage. ' kwh':$this->mileage. ' kmpl') :'',
+        'engine_capacity' => $this->engine_capacity
+            ? $this->engine_capacity . ' ' . $engineUnit
+            : '',
+        'mileage' => $this->mileage
+            ? $this->mileage . ' ' . $mileageUnit
+            : '',
         'ex_showroom_price' => 'KWD ' . $this->ex_showroom_price,
         'on_road_price' =>  $this->on_road_price ? 'KWD ' . $this->on_road_price :'',
         'price_in_lakh' => 'KWD '. $this->ex_showroom_price,
