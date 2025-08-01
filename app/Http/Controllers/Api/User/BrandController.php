c3ca68b20ab05222a278f953f2f4eb90cd32c67f
Nabeel Cryf T <nabeel.cryf@mindster.com>
2025-08-01 18:42:04 +0530

    removed pagination

--- app/Http/Controllers/Api/User/BrandController.php
+++ app/Http/Controllers/Api/User/BrandController.php
@@ -12,10 +12,10 @@ use Illuminate\Support\Facades\DB;
 
 class BrandController extends ApiBaseController
 {
-    
+
 public function __invoke(Request $request)
     {
-       
+
         $brands = Brand::active()
             ->when($request->has('search') && !empty($request->input('search')), function ($query) use ($request) {
                 $query->where('name', 'like', '%' . $request->input('search') . '%');
@@ -30,10 +30,10 @@ public function __invoke(Request $request)
             })
             ->when($request->is_top_brand, function($query, $value) {
                 $query->where('brands.is_top_brand', Brand::TOP_BRAND);
-            })
-            
-         
-            ->paginate(50);
+            });
+
+
+            // ->paginate(50);
         BrandResource::collection($brands);
         return $this->success(['data' => $brands], 'Brand listing!', Response::HTTP_OK);
     }
@@ -44,8 +44,8 @@ public function __invoke(Request $request)
             $query->select(DB::raw('SUM(view_count)'));
         }])
         ->orderBy('total_views', 'desc') // Order brands by total views in descending order
-        ->paginate(50); // Paginate results 
+        ->paginate(50); // Paginate results
         return $this->success(['data' => BrandResource::collection($popularBrands)], 'Popular Brand listing!', Response::HTTP_OK);
     }
-    
+
 }
