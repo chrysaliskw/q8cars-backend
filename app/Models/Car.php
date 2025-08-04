de8543730d4e2f0d7305903880269114765d77f3
Nabeel Cryf T <nabeel.cryf@mindster.com>
2025-08-04 13:48:02 +0530

    undo

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -42,7 +42,7 @@ class Car extends Model
     */
     public function offer()
     {
-        return $this->belongsTo(Offer::class, 'car_id', 'id');
+        return $this->belongsTo(Offer::class, 'id', 'car_id');
     }
     public function brand()
     {
