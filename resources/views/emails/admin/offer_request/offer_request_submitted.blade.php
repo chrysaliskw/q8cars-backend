e8f7bf63f697c519896cc6c8b860c6eaace201cb
Nabeel Cryf T <nabeel.cryf@mindster.com>
2025-08-05 18:14:44 +0530

    test

--- resources/views/emails/admin/offer_request/offer_request_submitted.blade.php
+++ resources/views/emails/admin/offer_request/offer_request_submitted.blade.php
@@ -27,7 +27,7 @@ Thank you for choosing us!
                                 <td style="background-color: #F9F9F9;padding:15px;">
 
                                     <a href="">
-                                        <img src="http://15.185.44.189/images/user_default.jpg">
+                                        <img src="http://15.185.44.189/images/Q8.png">
                                     </a>
                                 </td>
                             </tr>
